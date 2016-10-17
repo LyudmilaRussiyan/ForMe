3c22ae498a0393b1056be9f25fa15f3fe8bed3c3
LyudmilaRussiyan <lalka_best@ukr.net>
2016-10-17 15:00:30 +0300

    Create lesson4task2.java

---
+++ lesson4task2.java
@@ -0,0 +1,12 @@
+//Вывести на экран все числа от 1 до 100, которые делятся на 3 без остатка.
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+
+        for(int j=3; j<100; j=j+3)
+            System.out.println(j);
+
+    }
+}
